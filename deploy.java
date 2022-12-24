217bd5be89eb280d4d031316da03e76a2d715f2e
lokeshsgithub <lokeshreddy05690@gmail.com>
2022-12-24 12:41:10 +0530

    updating the commit files

--- deploy.java
+++ deploy.java
@@ -1,3 +1,4 @@
 this is the file contain the deployment files
 ths is the deployment file
-this is the file contain the deployment tools like jenkins 
+this is the file contain the deployment tools like jenkins
+the files use to learn deployment tools
