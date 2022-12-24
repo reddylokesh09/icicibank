e619e401c7082376e27559a6b764e1f1f1328664
Lokesh <reddylokesh09650@gmail.com>
2022-12-24 16:27:07 +0530

    Update deploy.java

--- deploy.java
+++ deploy.java
@@ -1,5 +1,4 @@
 this is the file contain the deployment files
 ths is the deployment file
 this is the file contain the deployment tools like jenkins
-the files use to learn deployment tools
 Maven web application is useful for all devops engineers
